2561c3195e7d714502ca631dbbfce78243db06e3
unknown <rankpleo@gmail.com>
2014-06-15 11:32:40 -0500

    'alsñfjsasdf'

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -11,13 +11,13 @@
  * file.
  */
 define('DB_NAME', $_ENV['OPENSHIFT_APP_NAME']);
-
+ 
 /** MySQL database username */
 define('DB_USER', $_ENV['OPENSHIFT_MYSQL_DB_USERNAME']);
-
+ 
 /** MySQL database password */
 define('DB_PASSWORD', $_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']);
-
+ 
 /** MySQL hostname */
 define('DB_HOST', $_ENV['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_ENV['OPENSHIFT_MYSQL_DB_PORT']);
 
@@ -26,7 +26,7 @@ define('DB_HOST', $_ENV['OPENSHIFT_MYSQL_DB_HOST'] . ':' . $_ENV['OPENSHIFT_MYSQ
 return array(
     'db' => array(
 		'driver' => 'Pdo',
-		'dsn' => 'mysql:dbname=php;'.DB_HOST,
+		'dsn' => 'mysql:dbname='.DB_NAME.';'.DB_HOST,
 		'username' => DB_USER,
 		'passoword' => DB_PASSWORD,		
 		'driver_options' => array(
